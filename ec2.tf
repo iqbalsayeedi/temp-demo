1b96f6f99c597f442007fe00f27a16420eef6a9d
iqbalsayeedi <100343595+iqbalsayeedi@users.noreply.github.com>
2023-08-06 06:23:55 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-  access_key = "AKIASKOYYLYRGCHKBXOC"
-  secret_key = "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"
-}
-
 resource "aws_instance" "myec2" {
    ami = "ami-0f34c5ae932e6f0e4"
    instance_type = "t2.micro"
